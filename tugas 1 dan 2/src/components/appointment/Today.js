d03357a70dcaff87a311063099983b4d361d8d8b
mayunswanitha <61812412+mayunswanitha@users.noreply.github.com>
2020-06-14 17:13:14 +0800

    tugas_react
    visual react

---
+++ tugas 1 dan 2/src/components/appointment/Today.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+class Today extends Component {
+    render() {
+        return (
+            <div>
+                <h3>ini Today</h3>
+            </div>
+        )
+    }
+}
+
+export default Today
